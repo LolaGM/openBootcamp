d9f177e2c00aedc9321863bfc80139395f96598c
LolaGM <canal030w@gmail.com>
2023-01-31 17:22:12 +0100

    openBootcamp exercises 1 and theory

---
+++ proyectoNode/index.js
@@ -0,0 +1,7 @@
+//este es el mensaje de bienvenida cuando ejecutas en terminal npm start que está en el archivo package.json
+console.log("Esta es la puerta de entrada al proyecto");
+
+/*barra asterisco para comentarios de más de una línea*/
+
+//este es el mensaje de despedida
+console.log("Adiós");
